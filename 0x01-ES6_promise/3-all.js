4f679530624f854bddf3b2f6a624f86933126fab
sakachris <sakachris90@gmail.com>
2024-01-03 21:15:29 +0300

    multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,12 @@
 // eslint-disable-next-line import/extensions
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photoResult, userResult] = results;
+    .then(([photoResult, userResult]) => {
       console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
     })
-    .catch(() => {
-      console.error('Signup system offline');
+    .catch((error) => {
+      console.error('Signup system offline', error);
     });
 }
-
-export default handleProfileSignup;
