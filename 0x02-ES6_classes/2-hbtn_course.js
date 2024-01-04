c660c3d112b27a0c6e6ee7bec75d9f66052ce8a0
sakachris <sakachris90@gmail.com>
2024-01-04 07:58:33 +0300

    classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+}
