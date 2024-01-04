e23116d7d0a6c48c8cfc361f4f2ba8e56c9f4397
sakachris <sakachris90@gmail.com>
2024-01-04 20:16:36 +0300

    es6 classes

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,26 @@
+class Car {
+    constructor(brand, motor, color) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    get brand() {
+        return this._brand;
+    }
+
+    get motor() {
+        return this._motor;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    cloneCar() {
+        const constructor = this.constructor;
+        return new constructor();
+    }
+}
+
+export default Car;
