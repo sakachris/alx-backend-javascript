b5392700f17c9a05aeaeb036420fb9b54c3438d8
sakachris <sakachris90@mail.com>
2024-02-26 20:04:17 +0300

    console

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+
+  if (name) {
+    console.log(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
