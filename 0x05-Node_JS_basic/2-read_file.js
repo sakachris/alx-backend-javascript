4a10e600a96fdaa45f20be5ae1a9e65c71e2a451
sakachris <sakachris90@mail.com>
2024-02-27 07:25:38 +0300

    reading file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.trim().split('\n');
+    const fieldNames = lines.shift().split(',');
+    const counters = {};
+
+    lines.forEach((line) => {
+      const [firstname, lastname, age, field] = line.split(',');
+
+      if (firstname && lastname && age && field) {
+        counters[field] = (counters[field] || 0) + 1;
+      }
+    });
+
+    console.log(`Number of students: ${lines.length}`);
+
+    for (const field in counters) {
+      const studentsList = lines
+        .filter((line) => line.split(',')[fieldNames.indexOf('field')] === field)
+        .map((line) => line.split(',')[fieldNames.indexOf('firstname')]);
+      console.log(`Number of students in ${field}: ${counters[field]}. List: ${studentsList.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+module.exports = countStudents;
