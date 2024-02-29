ef139852554689aaf9881542c7422685b8dc1a7a
sakachris <sakachris90@mail.com>
2024-02-29 15:47:30 +0300

    fix task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,19 @@
 const request = require('request');
 const { expect } = require('chai');
 
-const app = require('./api');
+// const app = require('./api');
 
 describe('Index page', () => {
-  it('Correct status code?', (done) => {
-    request.get('http://localhost:7865', (error, response) => {
+  it('Correct GET/ Response', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
       if (error) return done(error);
-      expect(response.statusCode).to.equal(200);
+      expect(response.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
 
-  it('Correct result?', (done) => {
+  /*it('Correct result?', (done) => {
     request.get('http://localhost:7865', (error, response, body) => {
       if (error) return done(error);
       expect(body).to.equal('Welcome to the payment system');
@@ -34,5 +35,5 @@ describe('Index page', () => {
       expect(response.statusCode).to.equal(404);
       done();
     });
-  });
+  });*/
 });
